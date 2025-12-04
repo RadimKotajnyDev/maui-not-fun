0b951033416fec73bfff334142fc34202f9657d1
Radim Kotajny <radimkotajny@gmail.com>
2025-12-04 08:56:15 +0100

    init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiMacApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiMacApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiMacApp.Pages")]
